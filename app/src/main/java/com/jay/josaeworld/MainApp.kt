c1cfaf9bd36d3d69134a387d13448b4af6591c7c
Jay-lol <krgeo@naver.com>
2022-03-08 01:53:58 +0900

    Fix warining in MainApp.kt

--- app/src/main/java/com/jay/josaeworld/MainApp.kt
+++ app/src/main/java/com/jay/josaeworld/MainApp.kt
@@ -3,7 +3,6 @@ package com.jay.josaeworld
 import android.app.Application
 import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
-import io.reactivex.disposables.Disposable
 import io.reactivex.rxjava3.exceptions.UndeliverableException
 import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import java.io.IOException
@@ -11,15 +10,11 @@ import java.net.SocketException
 
 @HiltAndroidApp
 class MainApp : Application() {
-    companion object {
-        lateinit var disposable: Disposable
-    }
-
     override fun onCreate() {
         super.onCreate()
 
         RxJavaPlugins.setErrorHandler { e ->
-            var error = e
+            var error: Throwable? = e
             if (error is UndeliverableException) {
                 error = e.cause
             }
@@ -34,13 +29,13 @@ class MainApp : Application() {
             if (error is NullPointerException || error is IllegalArgumentException) {
                 // that's likely a bug in the application
                 Thread.currentThread().uncaughtExceptionHandler
-                    .uncaughtException(Thread.currentThread(), error)
+                    ?.uncaughtException(Thread.currentThread(), error)
                 return@setErrorHandler
             }
             if (error is IllegalStateException) {
                 // that's a bug in RxJava or in a custom operator
                 Thread.currentThread().uncaughtExceptionHandler
-                    .uncaughtException(Thread.currentThread(), error)
+                    ?.uncaughtException(Thread.currentThread(), error)
                 return@setErrorHandler
             }
             Log.w("로그", "Undeliverable exception received, not sure what to do $error")
